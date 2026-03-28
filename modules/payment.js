148cfb26f9a230419e4bd072fe11a1bf12207ddf
murugannagaraja781 <murugannagaraja781@gmail.com>
2026-03-28 09:52:48 +0530

    Update payment.js

--- modules/payment.js
+++ modules/payment.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
-const axios = require('axios');
 const { User, Payment, BillingLedger } = require('../models');
 
 module.exports = function(io, shared) {
